936d6b6c60359ded2427e73bdf9e4eaab849fb0c
https://github.com/urjiiko1/code-craft/commits?author=urjiiko1 <gemachistesfaye36@gmail.com>
2025-08-28 10:10:18 -0900

    added simple greeting function

---
+++ Python Data Analysis/lesson2.py
@@ -0,0 +1,12 @@
+# here is function code
+
+def greet(name):
+    return f"Hello {name}, welcome to Ethiopian Airlines!"
+print(greet("Gemachis Tesfaye"))
+print(greet("Donald Trump😁"))   #this is simple greeting function only  use print for different passengers
+print(greet("Elon Musk🚀"))
+print(greet("Barack Obama"))
+print(greet("Bill Gates💻"))
+
+print("          ")
+# calculate ticket price after dsicount
