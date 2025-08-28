54a978bd6e614a3a93ec79f054ce683ef951282f
https://github.com/urjiiko1/code-craft/commits?author=urjiiko1 <gemachistesfaye36@gmail.com>
2025-08-27 23:08:26 -0900

    added new lesson which is list

--- Python Data Analysis/lesson1.py
+++ Python Data Analysis/lesson1.py
@@ -15,10 +15,23 @@ print(airlines, "have", passengers, "passengers that departure from", from_city,
 ## OUTPUT IS
 # Ethiopian Airlines have 170 passengers that departure from Addis Ababa into Hawassa the total price for this travel is $50.3 which flight is start Morning at 2:25 LT and arrived at 3:05 LT is found =  True
 
+print("          ")
+print("                   ")
+# next is list or commonly known as collecton of items
 
-
-
-
+passengers_names=["Gemachis Tesfaye", "Sisaay Tasew", "Bonsa Horsa", "Samuel Tilahun", "Daniel Alemayehu"]
+print(passengers_names[0])
+print(passengers_names[1])
+print(passengers_names[2])
+print(passengers_names[3])
+print(passengers_names[4])
+print(passengers_names)
+print(type(passengers_names))
+print(len(passengers_names))
+passengers_names.append("Abebe Guta")
+print(passengers_names)
+passengers_names.remove("Gemachis Tesfaye")
+print(passengers_names)
 
 
 
