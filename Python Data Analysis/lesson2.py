d4bd848796b182824ae92b1fd4c696d96e16bc56
https://github.com/urjiiko1/code-craft/commits?author=urjiiko1 <gemachistesfaye36@gmail.com>
2025-08-28 10:21:40 -0900

    added function that calculate ticket price with discount

--- Python Data Analysis/lesson2.py
+++ Python Data Analysis/lesson2.py
@@ -10,3 +10,10 @@ print(greet("Bill Gates💻"))
 
 print("          ")
 # calculate ticket price after dsicount
+def calculate(price, discount):
+    final_price=price-(price*discount/100)
+    return final_price
+
+print("The final price after discount is $", calculate(100,10))
+print("The final price after discount is $", calculate(50.3,5))
+print("The final price after discount is $", calculate(200,15))
