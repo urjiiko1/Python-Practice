4f6fd653b19bbe0342041a4f9b012f1bda221995
https://github.com/urjiiko1/code-craft/commits?author=urjiiko1 <gemachistesfaye36@gmail.com>
2025-08-27 23:56:18 -0900

    added mini data sets

--- Python Data Analysis/lesson1.py
+++ Python Data Analysis/lesson1.py
@@ -72,8 +72,10 @@ print("                   ")
 
 Passenger= [
     {"name": "Gemachis Tesfaye", "Age":21, "Seat":"12A", "Destination":"Adama"},
-    {"name":"Sisaay Tasew", "Age": 22, "Seat":"17B", "Destination":"Bishoftu"}
-    {"name":""}
+    {"name":"Sisaay Tasew", "Age": 22, "Seat":"17B", "Destination":"Bishoftu"},
+    {"name":"Bonsa Tilahun", "Age":23, "Seat":"10C", "Destination": "Mojo"},
+    {"name":"Samuel Tilahun", "Age": 21, "Seat":"9B", "Destination":"Addis"},
+    {}
 ]
 
 
