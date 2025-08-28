5a57d6a54a25acfb08bb578d27045894a55bb366
https://github.com/urjiiko1/code-craft/commits?author=urjiiko1 <gemachistesfaye36@gmail.com>
2025-08-27 22:04:53 -0900

    basic varibale and data type

---
+++ Python Data Analysis/lesson1.py
@@ -0,0 +1,24 @@
+airlines="Ethiopian Airlines"   ##string
+passengers=170                   ##integer
+from_city ="Addis Ababa"          ##string
+to_city="Hawassa"               ##string
+price="$50.3"                   ##float
+date="August 27, 2025"          ##string
+time="Morning"                  ##string
+departure_time="2:25 LT"        ##string
+arrival_time="3:05 LT"          ##string
+is_flight_on_time=True          ##boolean
+
+print(airlines, "have", passengers, "passengers that departure from", from_city, "into", to_city, 
+      "the total price for this travel is" , price, "which flight is start Morning at" , departure_time, "and arrived at" , arrival_time, "is found = " ,is_flight_on_time)
+
+
+
+
+
+
+
+
+
+
+
