dedfbd72b47d612b075984c6b5dd3c14acb5823e
https://github.com/urjiiko1/code-craft/commits?author=urjiiko1 <gemachistesfaye36@gmail.com>
2025-08-28 18:16:06 -0900

    added default parameters

--- Python Data Analysis/lesson2.py
+++ Python Data Analysis/lesson2.py
@@ -9,7 +9,7 @@ print(greet("Barack Obama"))
 print(greet("Bill Gates💻"))
 
 print("          ")
-# calculate ticket price after dsicount
+# calculate ticket price after dscount
 def calculate(price, discount):
     final_price=price-(price*discount/100)
     return final_price
@@ -17,3 +17,11 @@ def calculate(price, discount):
 print("The final price after discount is $", calculate(100,10))
 print("The final price after discount is $", calculate(50.3,5))
 print("The final price after discount is $", calculate(200,15))
+
+print("          ")
+def greet(name, message="welcome to Ethiopain Airlines!"):
+    return f"Hello {name}, {message}"
+
+print(greet("Gemachis Tesfaye"))   #output is Hello Gemachis Tesfaye, welcome to Ethiopain Airlines!
+print(greet("Donald Trump😁", "we are happy to have you onboard!"))   #output is Hello Donald Trump😁, we are happy to have you onboard!
+print(greet("Elon Musk🚀", "enjoy your flight with us!"))   #output is Hello Elon Musk🚀, enjoy your flight with us!
